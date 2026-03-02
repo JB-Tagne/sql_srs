59a5766d3176882316c32f9299728a1e7a8ca3ec
John B <johnborel91@gmail.com>
2026-03-02 15:31:50 +0100

    Première commit du fichier python de l'apply

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import duckdb
+
+
+st.write("My SQL App Project")
+data = {"a": [1, 2, 3], "b": [4, 5, 6]}
+df = pd.DataFrame(data)
+
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+    sql_query = st.text_area(label="Entrez votre texte")
+    result = duckdb.query(sql_query).df()
+    st.write(f"Vous avez entré la variable suivante {sql_query}")
+    st.dataframe(result)
+
